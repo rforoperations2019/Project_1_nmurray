bd29a7531a7651aa3eea81d13ef569563991ca0b
normur <54540112+normur@users.noreply.github.com>
2019-09-27 10:55:42 -0400

    Add 3rd plot
    State average line not added

--- app.R
+++ app.R
@@ -24,44 +24,50 @@ sidebar <- dashboardSidebar(
     menuItem("Plot", icon = icon("bar-chart"), tabName = "plot"),
     menuItem("Table", icon = icon("table"), tabName = "table"),
     menuItem("Dashboard", icon = icon("user-astronaut"), tabName = "Third tab"),
-  
-
-  # Inputs: county -----------------------------------------------------------------
-  pickerInput("countySelect",
-              label =  "Select County:",
-              choices = sort(levels(crimes$County)),
-              options = list(`actions-box` = TRUE),
-              multiple = T, 
-              selected = "Erie"),
-  
-  # Select Date Range for Filter by year (Over a Interval)---------------------------
-  sliderInput(inputId = "year_range", label = "Select Year Range:", 
-              min = 1990, max = 2018, value = c(1990, 2018), ticks = TRUE, sep = "", 
-              animate = animationOptions(interval = 500)), # Animates over time--Not sure I want this? 
-  
-  # Select Population Range-------------------------------------------------------------
-  sliderInput(inputId = "pop_range", label = "Select Counties with Population Size of:", 
-              min = 0, max = max(crimes$Population), value = c(min(crimes$Population),max(crimes$Population)), ticks = TRUE), 
-  
-  
-  # Select Crime Type (RATE) ------------------------------------------------------------------
-  radioButtons(inputId = "crime_type", 
-               label = "Select Type of Crime Rate: ",
-               choices = c("Firearms" = "Firearm.Rate",
-                           "Violent Crimes" = "Violent.Rate",
-                           "Index Crimes" = "Index.Rate",
-                           "Property Crimes" = "Property.Rate"),
-               selected = "Firearm.Rate"), 
-  
-  # Select Crime Type (Count) ------------------------------------------------------------------
-  radioButtons(inputId = "crime_count", 
-               label = "Select Type of Crime Count: ",
-               choices = c("Firearms" = "Firearm.Count",
-                           "Violent Crimes" = "Violent.Count",
-                           "Index Crimes" = "Index.Count",
-                           "Property Crimes" = "Property.Count"),
-               selected = "Firearm.Count") 
-  
+    
+    
+    # Inputs: county -----------------------------------------------------------------
+    pickerInput("countySelect",
+                label =  "Select County:",
+                choices = sort(levels(crimes$County)),
+                options = list(`actions-box` = TRUE),
+                multiple = T, 
+                selected = "Erie"),
+    
+    # # Select Date Range for Filter by year (Over a Interval)---------------------------
+    # sliderInput(inputId = "year_range", label = "Select Year Range:", 
+    #             min = 1990, max = 2018, value = c(1990, 2018), ticks = TRUE, sep = "", 
+    #             animate = animationOptions(interval = 500)), # Animates over time--Not sure I want this? 
+    
+    # SelectYEAR---------------------------
+    selectInput("yearSelect",
+                label =  "Select Year:",
+                choices = c(seq(1990,2018, by = 1)),
+                selected = "1990"),
+    
+    # Select Population Range-------------------------------------------------------------
+    sliderInput(inputId = "pop_range", label = "Select Counties with Population Size of:", 
+                min = 0, max = max(crimes$Population), value = c(min(crimes$Population),max(crimes$Population)), ticks = TRUE), 
+    
+    
+    # Select Crime Type (RATE) ------------------------------------------------------------------
+    radioButtons(inputId = "crime_type", 
+                 label = "Select Type of Crime Rate: ",
+                 choices = c("Firearms" = "Firearm.Rate",
+                             "Violent Crimes" = "Violent.Rate",
+                             "Index Crimes" = "Index.Rate",
+                             "Property Crimes" = "Property.Rate"),
+                 selected = "Firearm.Rate"), 
+    
+    # Select Crime Type (Count) ------------------------------------------------------------------
+    radioButtons(inputId = "crime_count", 
+                 label = "Select Type of Crime Count: ",
+                 choices = c("Firearms" = "Firearm.Count",
+                             "Violent Crimes" = "Violent.Count",
+                             "Index Crimes" = "Index.Count",
+                             "Property Crimes" = "Property.Count"),
+                 selected = "Firearm.Count") 
+    
   )
 )
 
@@ -76,14 +82,18 @@ body <- dashboardBody(tabItems(
   
   # Plot Page ------------------------------------------
   tabItem("plot",
-        fluidPage(
-          box(title = "Selected Crime Rate in Selected NYS Counties", width = 10,
-            plotlyOutput(outputId = "plot_firearmrt"))
-      ),
-      fluidPage(
-        box(title = "Raw Crime Count By Population", width = 10,
-            plotlyOutput(outputId = "scatter_crimepop"))
-      )
+          fluidPage(
+            box(title = "Selected Crime Rate in Selected NYS Counties", width = 10,
+                plotlyOutput(outputId = "plot_firearmrt"))
+          ),
+          fluidPage(
+            box(title = "Selected Crime Count By Population", width = 10,
+                plotlyOutput(outputId = "scatter_crimepop"))
+          ),
+          fluidPage(
+            box(title = "County Crime Rate Compared to State Average", width = 10,
+                plotlyOutput(outputId = "deviation"))
+          )
   ),
   
   # Dashboard---------------------
@@ -102,79 +112,85 @@ ui <- dashboardPage(skin = "green", header, sidebar, body)
 
 server <- function(input, output) {
   
-  # #Create a subset to filter by Year And County and Population------------------------------------
-  year_CountySubset <- reactive({
-      req(input$year_range, input$countySelect, input$pop_range)
-      data_subset <- crimes[(crimes['Year'] >= input$year_range[1]) & (crimes['Year'] <= input$year_range[2]) &
-                             crimes$County %in% input$countySelect &
-                              (crimes['Population'] >= input$pop_range[1]) & (crimes['Population'] <= input$pop_range[2]),]
-    data_subset
-
-  })
-  # Subset to Filter by County and Population
-  county_popSubset <-reactive({
-    req(input$countySelect,input$pop_range)
+  # #Create a subset to filter by And County and Population------------------------------------
+  yearrange_CountySubset <- reactive({
+    req(input$countySelect, input$pop_range)
     data_subset <- crimes[crimes$County %in% input$countySelect &
                             (crimes['Population'] >= input$pop_range[1]) & (crimes['Population'] <= input$pop_range[2]),]
     data_subset
     
   })
-  
-  # Subset to Filter by County ONLY ---------------------------------------------------
-  CountySubset <- reactive({
-      req(input$countySelect)
-      data_subset <- crimes[crimes$County %in% input$countySelect,]
-      data_subset
-  })
-  
-  # Subset to Filter by Population Size ONLY-----------------------------------------------
-  popSubset <- reactive({
-    req(input$pop_range)
-    data_subset <- crimes[crimes$Population %in% input$pop_range,]
+
+  # #Create a subset to filter by SINGLE Year And County and Population------------------------------------
+  year_CountySubset <- reactive({
+    req(input$yearSelect, input$countySelect, input$pop_range)
+    data_subset <- crimes[crimes$Year %in% input$yearSelect &
+                            crimes$County %in% input$countySelect &
+                            (crimes['Population'] >= input$pop_range[1]) & (crimes['Population'] <= input$pop_range[2]),]
     data_subset
+    
   })
   
-  # Subset to Filter by Crime Type  ---------------------------------------
-  firearmsSubset <- reactive({
-      req(input$crime_type)
-      data_subset <-filter(crimes, title_type %in% input$selected_type)
+  
+  # Data table ----------------------------------------------
+  output$table <- DT::renderDataTable({
+    yearrange_CountySubset()
   })
   
-
- # Data table ----------------------------------------------
- output$table <- DT::renderDataTable({
-     year_CountySubset()
-   })
- 
- 
- # Bar PLot ----------------------------  Crime  Rate Type by County
- output$plot_firearmrt <- renderPlotly({
-      g <-ggplot(year_CountySubset(), aes_string(x ="Year", y = input$crime_type, fill = "County")) +
+  
+  # Bar PLot ----------------------------  Crime  Rate Type by County
+  output$plot_firearmrt <- renderPlotly({
+    g <-ggplot(yearrange_CountySubset(), aes_string(x ="Year", y = input$crime_type, fill = "County")) +
       geom_bar(stat = 'identity') +
-      labs(title=paste( input$crime_type, "Crimes in", input$countySelect, sep = " "), x="Year")
-
-      ggplotly(g)
- })
+      labs(title=paste(input$crime_type, "Crimes Rates", sep = " "), x="Year")
+    
+    ggplotly(g)
+  })
+  
+  # Scatter Plot------------------------ Raw Crime Count By Population
+  
+  output$scatter_crimepop <- renderPlotly({
+    p <- ggplot(year_CountySubset(), aes_string(x = "Population", y = input$crime_count, fill ="County")) + 
+      geom_point() + labs(title=paste(input$crime_count, "Crimes Counts", sep = " "), x="Population", y = "input$crime_count") +
+      theme(axis.text.x = element_text(angle = 45)) +
+      facet_wrap("Year")
+    
+    
+    ggplotly(p)
+    
+    
+  })
+  
+  # Diverging Bar Chart------------------------ County Crime Rate Compared to State Average
+  
+  output$deviation <- renderPlotly({
+    
+    # z_score <- round((input$crime_type - mean(input$crime_type))/sd(input$crime_type), 2)
+    # crimes$above_below <- ifelse(crimes$z_score < 0, "below", "above")
  
- # Scatter Plot------------------------ Raw Crime Count By Population
- 
- output$scatter_crimepop <- renderPlotly({
-   p <- ggplot(year_CountySubset(), aes_string(x = "Population", y = input$crime_count, fill ="County")) + 
-     geom_point() + labs(title=paste( input$crime_count, "Crimes in", input$countySelect, sep = " "), x="Population", y = "input$crime_count") +
-     theme(axis.text.x = element_text(angle = 45)) +
-     facet_wrap("Year")
+    
+    state_avg <- mean(input$crime_type)
      
+    # Diverging Barcharts
+   q <- ggplot(year_CountySubset(), aes_string(x="County", y=input$crime_type, label=input$crime_type, fill = "County")) +
+      geom_bar(stat='identity', width=.5) +
+     # scale_colour_manual(name = 'Above State Average', values = setNames(c('red','green'))) +
+      labs(subtitle="Rename'",
+           title= paste(input$crime_type, "in Selected Counties Compared to State Average")) +
+      coord_flip() +
+     facet_wrap("Year") +
+     geom_vline(xintercept = state_avg)
+   
   
-   ggplotly(p)
-
-
- })
- 
- 
+    
+  ggplotly(q)
   
-
+  })
+  
+  
+  
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
