f7c47a9f74647de4d7275aa8d794fdd68c0f4677
normur <54540112+normur@users.noreply.github.com>
2019-09-26 22:43:12 -0400

    Add Scatter plot

--- app.R
+++ app.R
@@ -7,6 +7,7 @@ library(tools)
 library(stringr)
 library(shinythemes)
 library(shinyWidgets)
+library(ggalt)
 
 # Load Data---------
 crimes <- data.frame(read.csv("Index__Violent__Property__and_Firearm_Rates_By_County__Beginning_1990.csv"))
@@ -43,14 +44,23 @@ sidebar <- dashboardSidebar(
               min = 0, max = max(crimes$Population), value = c(min(crimes$Population),max(crimes$Population)), ticks = TRUE), 
   
   
-  # Select Crime Type ------------------------------------------------------------------
+  # Select Crime Type (RATE) ------------------------------------------------------------------
   radioButtons(inputId = "crime_type", 
                label = "Select Type of Crime Rate: ",
                choices = c("Firearms" = "Firearm.Rate",
                            "Violent Crimes" = "Violent.Rate",
                            "Index Crimes" = "Index.Rate",
                            "Property Crimes" = "Property.Rate"),
-               selected = "Firearm.Rate")
+               selected = "Firearm.Rate"), 
+  
+  # Select Crime Type (Count) ------------------------------------------------------------------
+  radioButtons(inputId = "crime_count", 
+               label = "Select Type of Crime Count: ",
+               choices = c("Firearms" = "Firearm.Count",
+                           "Violent Crimes" = "Violent.Count",
+                           "Index Crimes" = "Index.Count",
+                           "Property Crimes" = "Property.Count"),
+               selected = "Firearm.Count") 
   
   )
 )
@@ -59,15 +69,20 @@ body <- dashboardBody(tabItems(
   # Data Table Page -----------------------------------
   tabItem("table",
           fluidPage(
-            box(title = "Title of Data table", DT::dataTableOutput(outputId = "table"))
+            box(title = "Data Table: Filter by County, Population, and Year", titleWidth = 450, 
+                DT::dataTableOutput(outputId = "table"))
           )
   ),
   
   # Plot Page ------------------------------------------
   tabItem("plot",
         fluidPage(
-          box(title = "Plot--Not sure", width = 12,
-            plotOutput(outputId = "plot_firearmrt"))
+          box(title = "Selected Crime Rate in Selected NYS Counties", width = 10,
+            plotlyOutput(outputId = "plot_firearmrt"))
+      ),
+      fluidPage(
+        box(title = "Raw Crime Count By Population", width = 10,
+            plotlyOutput(outputId = "scatter_crimepop"))
       )
   ),
   
@@ -91,9 +106,18 @@ server <- function(input, output) {
   year_CountySubset <- reactive({
       req(input$year_range, input$countySelect, input$pop_range)
       data_subset <- crimes[(crimes['Year'] >= input$year_range[1]) & (crimes['Year'] <= input$year_range[2]) &
-                             crimes$County %in% input$countySelect,]
+                             crimes$County %in% input$countySelect &
+                              (crimes['Population'] >= input$pop_range[1]) & (crimes['Population'] <= input$pop_range[2]),]
     data_subset
 
+  })
+  # Subset to Filter by County and Population
+  county_popSubset <-reactive({
+    req(input$countySelect,input$pop_range)
+    data_subset <- crimes[crimes$County %in% input$countySelect &
+                            (crimes['Population'] >= input$pop_range[1]) & (crimes['Population'] <= input$pop_range[2]),]
+    data_subset
+    
   })
   
   # Subset to Filter by County ONLY ---------------------------------------------------
@@ -110,7 +134,7 @@ server <- function(input, output) {
     data_subset
   })
   
-  # Subset to Filter by Crime Type  ---------------------------------------NOT CURRENTLY REACTIVE 
+  # Subset to Filter by Crime Type  ---------------------------------------
   firearmsSubset <- reactive({
       req(input$crime_type)
       data_subset <-filter(crimes, title_type %in% input$selected_type)
@@ -123,14 +147,32 @@ server <- function(input, output) {
    })
  
  
- # Density Plot of Year by County ----------------------------  CHANGE TO PLOTLY, PLOT NAME CURRENT JUST 1
- output$plot_firearmrt <- renderPlot({
-   print(input$crime_type)
-      ggplot(CountySubset(), aes_string(x ="Year", y = input$crime_type )) +
-      geom_bar(stat = 'identity') + labs(title=paste( input$crime_type, "Crimes in", input$countySelect, sep = " "), x="Year")   ### Insert reactive 
- }
- )
+ # Bar PLot ----------------------------  Crime  Rate Type by County
+ output$plot_firearmrt <- renderPlotly({
+      g <-ggplot(year_CountySubset(), aes_string(x ="Year", y = input$crime_type, fill = "County")) +
+      geom_bar(stat = 'identity') +
+      labs(title=paste( input$crime_type, "Crimes in", input$countySelect, sep = " "), x="Year")
+
+      ggplotly(g)
+ })
+ 
+ # Scatter Plot------------------------ Raw Crime Count By Population
+ 
+ output$scatter_crimepop <- renderPlotly({
+   p <- ggplot(year_CountySubset(), aes_string(x = "Population", y = input$crime_count, fill ="County")) + 
+     geom_point() + labs(title=paste( input$crime_count, "Crimes in", input$countySelect, sep = " "), x="Population", y = "input$crime_count") +
+     theme(axis.text.x = element_text(angle = 45)) +
+     facet_wrap("Year")
+     
   
+   ggplotly(p)
+
+
+ })
+ 
+ 
+  
+
 }
 
 # Run the application 
