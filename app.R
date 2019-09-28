a9b93cbc4181070e5e43def8925ec2d29bb86f43
normur <54540112+normur@users.noreply.github.com>
2019-09-27 20:08:20 -0400

    Add Info Boxes
    Layout reorganized for infobox placement. 
    Shiny theme applied
    Data information page added.

--- app.R
+++ app.R
@@ -21,18 +21,18 @@ sidebar <- dashboardSidebar(
     id = "tabs",
     
     # Menu Items ----------------------------------------------
-    menuItem("Plot", icon = icon("bar-chart"), tabName = "plot"),
+    menuItem("Topline Plots", icon = icon("bar-chart"), tabName = "plot"),
     menuItem("Table", icon = icon("table"), tabName = "table"),
-    menuItem("Dashboard", icon = icon("user-astronaut"), tabName = "Third tab"),
-    menuItem("Crimes Rates Drilldown", icon = icon("cog"), tabName = "crimerates"),
+    menuItem("Dashboard", icon = icon("user-astronaut"), tabName = "tab"),
+    menuItem("Crime Rates Drilldown", icon = icon("cog"), tabName = "crimerates"),
     
     # Inputs: county -----------------------------------------------------------------
     pickerInput("countySelect",
-                label =  "Select County:",
+                label =  "Select Multiple Counties:",
                 choices = sort(levels(crimes$County)),
                 options = list(`actions-box` = TRUE),
                 multiple = T, 
-                selected = "Erie"),
+                selected = c("Erie", "Wyoming", "Orleans", "Bronx", "Hamilton", "Oneida")),
     
     # # Select Date Range for Filter by year (Over a Interval)---------------------------
     # sliderInput(inputId = "year_range", label = "Select Year Range:", 
@@ -74,8 +74,8 @@ sidebar <- dashboardSidebar(
 body <- dashboardBody(tabItems(
   # Data Table Page -----------------------------------
   tabItem("table",
-          fluidPage(
-            box(title = "Data Table: Filter by County, Population, and Year", titleWidth = 450, 
+          fluidPage(theme = shinytheme("yeti"),
+            box(title = "Data Table: Filter by County, Population, and Year", titleWidth = 600, 
                 DT::dataTableOutput(outputId = "table"))
           )
   ),
@@ -97,19 +97,51 @@ body <- dashboardBody(tabItems(
           fluidRow(
             tabBox(title = "Crime Rates",
                    width = 12,
-                   tabPanel("Index Rate", plotlyOutput(outputId = "plot1")),
-                   tabPanel("Violent Rate", plotlyOutput(outputId = "plot2")),
-                   tabPanel("Property Rate", plotlyOutput(outputId = "plot3")),
-                   tabPanel("Property Rate", plotlyOutput(outputId = "plot4"))
+                   tabPanel("Index Rate", plotlyOutput(outputId = "plot1", height=650)),
+                   tabPanel("Violent Rate", plotlyOutput(outputId = "plot2", height=650)),
+                   tabPanel("Property Rate", plotlyOutput(outputId = "plot3", height=650)),
+                   tabPanel("Firearms Rate", plotlyOutput(outputId = "plot4", height=650))
             )
           )
   ),
   
   # Dashboard---------------------
-  tabItem("Third tab", 
-          fluidPage(infoBox(title = "This is a title", value = NULL, subtitle = NULL,
-                            icon = shiny::icon("bar-chart"), color = "aqua", width = 4,
-                            href = NULL, fill = FALSE))
+  
+  # SOURCE: https://rstudio.github.io/shinydashboard/structure.html#boxes
+  tabItem("tab",
+          fluidRow(
+            column(width = 6,
+                   infoBoxOutput(outputId = "totalCrimes", 
+                                 width = NULL),
+                   infoBoxOutput(outputId = "avgFirearms",
+                                 width = NULL),
+                   infoBoxOutput(outputId = "avgViolent", 
+                                 width = NULL),
+                   infoBoxOutput(outputId = "avgProperty",
+                                 width = NULL)),
+            column(width = 6,
+                   infoBoxOutput(outputId = "avgIndRate", 
+                                 width = NULL),
+                   infoBoxOutput(outputId = "avgFireRate", 
+                                 width = NULL),
+                   infoBoxOutput(outputId = "avgVilRate",
+                                 width = NULL), 
+                   infoBoxOutput(outputId = "avgPropRate", 
+                                 width = NULL))
+          ),
+          fluidRow(
+            box(title = "Information on Data Set",width =12, height = 200, status = "success",
+                       "Date for this dashboard can be found on NY state's open data portal:
+                https://data.ny.gov/Public-Safety/Index-Violent-Property-and-Firearm-Rates-By-County/34dd-6g2j
+                
+                Note: The New York City Police Department does not report the number of violent crimes 
+                by firearm so those counts are not available for the five counties that comprise New York City. 
+                
+                Information on Key Terms: All rates are the number of crimes reported per 100,000 people and Index 
+                refers to the total number of crimes, including: Murder, Rape, 
+                Robbery, Aggravated Assault, Burglary, Larceny and Motor Vehicle Theft.")
+          )
+          
   )
 )
 )
@@ -139,13 +171,19 @@ server <- function(input, output) {
     data_subset
     
   })
-  
+ 
+  ########################### DATA TABLE ################################ 
   
   # Data table ----------------------------------------------
   output$table <- DT::renderDataTable({
-    yearrange_CountySubset()
+
+    
+    DT::datatable(data = yearrange_CountySubset(), options = list(
+      autoWidth = TRUE,
+      scrollY = 450))
   })
   
+  ########################### PLOTS ################################
   
   # Bar PLot ----------------------------  Crime  Rate Type by County
   output$plot_firearmrt <- renderPlotly({
@@ -178,8 +216,7 @@ server <- function(input, output) {
       labs(subtitle="Rename'",
            title= paste("Index Crime Rate in Selected Counties")) +
       coord_flip() +
-      facet_wrap("Year") +
-      geom_vline(xintercept = as.numeric(mean(year_CountySubset()$Index.Rate)))
+      facet_wrap("Year") 
     
     ggplotly(p1)
   })
@@ -192,8 +229,7 @@ server <- function(input, output) {
       labs(subtitle="Rename'",
            title= paste("Violent Crime Rate in Selected Counties")) +
       coord_flip() +
-      facet_wrap("Year") +
-      geom_vline(xintercept = as.numeric(mean(year_CountySubset()$Violent.Rate)))
+      facet_wrap("Year") 
     
     ggplotly(p2)
     
@@ -207,8 +243,7 @@ server <- function(input, output) {
         labs(subtitle="Rename'",
              title= paste("Property Crime Rate in Selected Counties")) +
         coord_flip() +
-        facet_wrap("Year") +
-        geom_vline(xintercept = as.numeric(mean(year_CountySubset()$Property.Rate)))
+        facet_wrap("Year") 
     
     ggplotly(p3)
   })
@@ -220,12 +255,88 @@ server <- function(input, output) {
       labs(subtitle="Rename'",
            title= paste("Firearm Crime Rate in Selected Counties")) +
       coord_flip() +
-      facet_wrap("Year") +
-      geom_vline(xintercept = as.numeric(mean(year_CountySubset()$Firearm.Rate)))
+      facet_wrap("Year") 
     
     ggplotly(p4)
     
   })
+  
+  
+  ########################### INFO BOXES #####################################################
+  # as.numeric(mean(year_CountySubset()$Firearm.Rate)))
+  
+  # Total # of Crimes in Year info box ----------------------------------------------
+  output$totalCrimes <- renderInfoBox({
+    data <- year_CountySubset()
+    sum <- sum(data$Index.Count)
+    infoBox(paste(title = "Total # of Crimes in ", input$yearSelect),
+            value = paste(sum, "Crimes"),
+            icon = icon("cog"), color = "red")
+  })
+  
+  # Avg # of Firearms Crimes in Year info box ----------------------------------------------
+  output$avgFirearms <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Firearm.Count),0)
+    infoBox(paste(title = "AVG # of Firearms Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("cog"), color = "green")
+  })
+  
+  # Avg # of Violent Crimes in Year info box ----------------------------------------------
+  output$avgViolent <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Violent.Count),0)
+    infoBox(paste(title = "AVG # of Violent Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("cog"), color = "green")
+  })
+  
+  # Avg # of Property Crimes in Year info box ----------------------------------------------
+  output$avgProperty <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Property.Count),0)
+    infoBox(paste(title = "AVG # of Property Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("cog"), color = "green")
+  })
+  
+  # Avg Rate of Firearms Crimes in Year info box ----------------------------------------------
+  output$avgFireRate <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Firearm.Rate),0)
+    infoBox(paste(title = "AVG Rate of Firearms Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("bell"), color = "purple")
+  })
+  
+  # Avg Rate of Violent Crimes in Year info box ----------------------------------------------
+  output$avgVilRate <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Violent.Rate),0)
+    infoBox(paste(title = "AVG Rate of Violent Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("bell"), color = "purple")
+  })
+  
+  # Avg Rate of Property Crimes in Year info box ----------------------------------------------
+  output$avgPropRate <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Property.Rate),0)
+    infoBox(paste(title = "AVG Rate of Property Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("bell"), color = "purple")
+  })
+
+  # Avg Rate of ALL  Crimes in Year info box ----------------------------------------------
+  output$avgIndRate <- renderInfoBox({
+    data <- year_CountySubset()
+    avg <- round(mean(data$Index.Rate),0)
+    infoBox(paste(title = "AVG Rate of All Crimes in ", input$yearSelect),
+            value = paste(avg, "Crimes"),
+            icon = icon("cog"), color = "red")
+  })
+  
   
 }
 
